4d94ca44d9eab33a0f74f1a218dc998e4c144ae3
wuyuan <2461190767@qq.com>
2024-02-12 10:40:13 +0800

    update

--- member/runtime/src/task/future.rs
+++ member/runtime/src/task/future.rs
@@ -2,10 +2,10 @@ use std::future::Future;
 
 /// 封装传入的future -- 内部重写poll函数由轮询线程调用
 pub struct FutureWrapper<T> {
+    // 内部维护传入future的对象指针
     future: *mut (dyn Future<Output=T> + 'static),
 }
 
-
-pub fn print_test(){
+pub fn print_test() {
     println!("------- print_test -------");
 }
